687cd4532b5b33d6348644d4225d055d9708848b
shuanvi7 <shuanvi7@gmail.com>
2026-07-24 16:46:34 +0300

    Dev header

--- src/widgets/theHeader/ui/TheHeader.vue
+++ src/widgets/theHeader/ui/TheHeader.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+import VButton from '@/shared/ui/VButton/VButton.vue'
 import { useRoute } from 'vue-router'
 
 const router = useRoute()
 </script>
 
 <template>
-  <header>
-    {{ router.meta.title }}
-    {{ router.meta.subtitle }}
+  <header class="flex items-center justify-between gap-3">
+    <div>
+      <h2 class="mb-1 text-[30px] font-bold">{{ router.meta.title }}</h2>
+      <div class="text-foreground-muted text-[14px]">{{ router.meta.subtitle }}</div>
+    </div>
+    <div class="flex items-center justify-between gap-3">
+      <input placeholder="Search tasks..." type="text" />
+      <VButton>+</VButton>
+      <VButton>Add Task</VButton>
+    </div>
   </header>
 </template>
 
